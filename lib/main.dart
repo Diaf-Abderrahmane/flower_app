00580cc67415f7fd2895819cea2fbd98c3e30f22
Shoyoo01 <ja_diaf@esi.dz>
2024-01-05 17:34:09 +0100

    firebase 1.1

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ import 'package:flower_app/pages/login.dart';
 import 'package:flower_app/pages/register.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(const MyApp());
